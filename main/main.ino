4cbe000f15efeca742a208e2aacaf630f62ea232
sgtkode <sgtkode01@gmail.com>
2016-03-21 14:06:12 -0400

    started work on input gathering

--- main/main.ino
+++ main/main.ino
@@ -9,26 +9,32 @@ int outputRows = 4;
 int outputColumns = 4;
 
 void setup() {
-  for (int i =0; i < (sizeof(ledPin)/sizeof(int)); i++)
-  {
-    pinMode(ledPin[i], OUTPUT);
+  for (int i =0; i < (sizeof(inputPins)/sizeof(int)); i++){
+    pinMode(inputPins[i], INPUT);
+  }
+  for (int i =0; i < (sizeof(outputPins)/sizeof(int)); i++){
+    pinMode(outputPins[i], OUTPUT);
   }
 }
 
 void loop() {
   // check inputs
-  for (int i =0; i < inputColumns; i++)
-  {
+  for (int i = 1; i < inputColumns; i++){
     // send power to column
-
+    for (int j = 0; j < (sizeof(inputPins)/sizeof(int)); j++){
+      if (bitRead(i, j) == 1){
+        digitalWrite(inputPins[j], HIGH); 
+      } else {
+        digitalWrite(outputPins[j], LOW); 
+      }
+    }
     // check input ports
 
     // write to input grid
   }
   
   // send outputs
-  for (int i =0; i < outputColumns; i++)
-  {
+  for (int i =0; i < outputColumns; i++){
     // send power to column
 
     // close circuit based on input values
