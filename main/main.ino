a6af87d3794b700c0008cfef865b4ade166d476a
sgtkode <sgtkode01@gmail.com>
2016-03-22 12:20:03 -0400

    finished gathering inputs code; started work on output sedning

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,10 @@
-int inputPins[]  = {1,2,3};
-int outputPins[] = {4,5,6};
-int powerPins[]  = {7,8,9};
+int inputRowPins[] = {1,2,3};
+int inputColumnPins[] = {4,5,6};
+int powerPins[] = {7,8,9};
 
 int inputRows = 3;
 int inputColumns = 3;
 int inputGrid[][];
-
-int outputRows = 3;
-int outputColumns = 3;
 
 void setup() {
   for (int i =0; i < (sizeof(inputPins)/sizeof(int)); i++){
@@ -16,32 +13,35 @@ void setup() {
   for (int i =0; i < (sizeof(outputPins)/sizeof(int)); i++){
     pinMode(outputPins[i], OUTPUT);
   }
+  for (int i =0; i < (sizeof(powerPins)/sizeof(int)); i++){
+    pinMode(powerPins[i], OUTPUT);
+  }
 }
 
 void loop() {
   // check inputs
   for (int i = 1; i < inputColumns; i++){
     // send power to column
-    for (int j = 0; j < (sizeof(outputPins)/sizeof(int)); j++){
-      if (bitRead(i, j) == 1){
-        digitalWrite(outputPins[j], HIGH);
-      } else {
-        digitalWrite(outputPins[j], LOW);
-      }
-    }
-
-    // check input ports
-    for (int j = 0; j < (sizeof(inputPins)/sizeof(int)); j++){
-      inputGrid[i][j] = digitalRead(inputPins[j]);;
-    }
+    digitalWrite(inputColumnPins[i], HIGH);
 
     // write to input grid
+    inputGrid[i][0] = digitalRead(inputRowPins[0]);
+    inputGrid[i][1] = digitalRead(inputRowPins[1]);
+    inputGrid[i][2] = digitalRead(inputRowPins[2]);
   }
 
   // send outputs
   for (int i =0; i < outputColumns; i++){
     // send power to column
-
+    for (int i = 1; i < inputColumns; i++){
+      // send power to column
+      digitalWrite(inputColumnPins[i], HIGH);
+  
+      // write to input grid
+      inputGrid[i][0] = digitalRead(inputRowPins[0]);
+      inputGrid[i][1] = digitalRead(inputRowPins[1]);
+      inputGrid[i][2] = digitalRead(inputRowPins[2]);
+    }
     // close circuit based on input values
   }
 }
