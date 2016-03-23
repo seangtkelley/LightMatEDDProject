1fac781a296837d3643979a241ee63fd257296bb
Sean Kelley <sgtkode01@gmail.com>
2016-03-23 13:08:55 -0400

    reformatted binaryCountTest; finished logic forr writing outputs

--- binaryCountTest/binaryCountTest.ino
+++ binaryCountTest/binaryCountTest.ino
@@ -1,33 +1,24 @@
 int ledPin[] = {2,3};
 
-void setup()
-{
-  for (int i =0; i < (sizeof(ledPin)/sizeof(int)); i++)
-  {
+void setup(){
+  for (int i =0; i < (sizeof(ledPin)/sizeof(int)); i++){
     pinMode(ledPin[i], OUTPUT);
   }
 }
 
-void loop() 
-{
-  for (byte counter =0;counter<=3; counter++)
-  {
+void loop(){
+  for (byte counter =0;counter<=3; counter++){
     displayBinary(counter);
-    delay(2000); 
+    delay(2000);
   }
 }
 
-void displayBinary(byte numToShow)
-{
-  for (int i =0; i < (sizeof(ledPin)/sizeof(int)); i++)
-  {
-    if (bitRead(numToShow, i)==1)
-    {
-      digitalWrite(ledPin[i], HIGH); 
-    }
-    else
-    {
-      digitalWrite(ledPin[i], LOW); 
+void displayBinary(byte numToShow){
+  for (int i =0; i < (sizeof(ledPin)/sizeof(int)); i++){
+    if (bitRead(numToShow, i)==1){
+      digitalWrite(ledPin[i], HIGH);
+    } else {
+      digitalWrite(ledPin[i], LOW);
     }
   }
 }
--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
 int inputRowPins[] = {1,2,3};
 int inputColumnPins[] = {4,5,6};
 int powerPins[] = {7,8,9};
+int switchPins[] = {10,11,12};
 
 int inputRows = 3;
 int inputColumns = 3;
@@ -20,7 +21,7 @@ void setup() {
 
 void loop() {
   // check inputs
-  for (int i = 1; i < inputColumns; i++){
+  for (int i = 0; i < inputColumns - 1; i++){
     // send power to column
     digitalWrite(inputColumnPins[i], HIGH);
 
@@ -31,17 +32,23 @@ void loop() {
   }
 
   // send outputs
-  for (int i =0; i < outputColumns; i++){
-    // send power to column
-    for (int i = 1; i < inputColumns; i++){
-      // send power to column
-      digitalWrite(inputColumnPins[i], HIGH);
-  
-      // write to input grid
-      inputGrid[i][0] = digitalRead(inputRowPins[0]);
-      inputGrid[i][1] = digitalRead(inputRowPins[1]);
-      inputGrid[i][2] = digitalRead(inputRowPins[2]);
+  for (int i =0; i < inputColumns - 1; i++){
+    // power column
+    for (int j = 0; j < (sizeof(powerPins)/sizeof(int)); j++){
+      if (bitRead(i, j)==1){
+        digitalWrite(powerPins[j], HIGH);
+      } else {
+        digitalWrite(powerPins[j], LOW);
+      }
     }
+
     // close circuit based on input values
+    for (int j = 0; j < inputRows - 1; j++){
+      if(inputGrid[i][j] == HIGH){
+        // close circuit
+      } else {
+        // open circuit
+      }
+    }
   }
 }
