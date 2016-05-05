fdb31826f0cde321baa6c5317f9b6dfb5f743907
Sean Kelley <sgtkode01@gmail.com>
2016-05-05 14:06:01 -0400

    fixed ports and array names

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,24 @@
-int inputRowPins[] = {1,2,3};
-int inputColumnPins[] = {4,5,6};
-int powerPins[] = {7,8,9};
-int switchPins[] = {10,11,12};
+int btnPowerPins = {6,7,8};     // pins to power rows of buttons
+int btnInputPins = {9,10,11};   // pins to revieve input from buttons
+int powerMux[] = {2,3};         // pins to power rows of LEDs
+int gndMux[] = {4,5};           // pins to ground columns of LEDs
 
 int inputRows = 3;
 int inputColumns = 3;
 int inputGrid[][];
 
 void setup() {
-  for (int i =0; i < (sizeof(inputPins)/sizeof(int)); i++){
-    pinMode(inputPins[i], INPUT);
+  for (int i =0; i < (sizeof(btnInputPins)/sizeof(int)); i++){
+    pinMode(btnInputPins[i], INPUT);
   }
-  for (int i =0; i < (sizeof(outputPins)/sizeof(int)); i++){
-    pinMode(outputPins[i], OUTPUT);
+  for (int i =0; i < (sizeof(btnPowerPins)/sizeof(int)); i++){
+    pinMode(btnPowerPins[i], OUTPUT);
   }
-  for (int i =0; i < (sizeof(powerPins)/sizeof(int)); i++){
-    pinMode(powerPins[i], OUTPUT);
+  for (int i =0; i < (sizeof(powerMux)/sizeof(int)); i++){
+    pinMode(powerMux[i], OUTPUT);
+  }
+  for (int i =0; i < (sizeof(gndMux)/sizeof(int)); i++){
+    pinMode(gndMux[i], OUTPUT);
   }
 }
 
@@ -23,12 +26,12 @@ void loop() {
   // check inputs
   for (int i = 0; i < inputColumns - 1; i++){
     // send power to column
-    digitalWrite(inputColumnPins[i], HIGH);
+    digitalWrite(btnPowerPins[i], HIGH);
 
     // write to input grid
-    inputGrid[i][0] = digitalRead(inputRowPins[0]);
-    inputGrid[i][1] = digitalRead(inputRowPins[1]);
-    inputGrid[i][2] = digitalRead(inputRowPins[2]);
+    inputGrid[i][0] = digitalRead(btnInputPins[0]);
+    inputGrid[i][1] = digitalRead(btnInputPins[1]);
+    inputGrid[i][2] = digitalRead(btnInputPins[2]);
   }
 
   // send outputs
@@ -46,6 +49,13 @@ void loop() {
     for (int j = 0; j < inputRows - 1; j++){
       if(inputGrid[i][j] == HIGH){
         // close circuit
+        for (int j = 0; j < (sizeof(powerPins)/sizeof(int)); j++){
+          if (bitRead(i, j)==1){
+            digitalWrite(powerPins[j], HIGH);
+          } else {
+            digitalWrite(powerPins[j], LOW);
+          }
+        }
       } else {
         // open circuit
       }
